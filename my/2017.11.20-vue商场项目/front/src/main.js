0443a12be485f39fc9ce33e9386f26892794a37e
461863712 <461863712@qq.com>
2017-11-21 17:11:47 +0800

    vue商城-x-shop

---
+++ my/2017.11.20-vue商场项目/front/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, Axios)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+  mounted () {
+    this.$http.get('/goods/list?page=1&pageSize=3&sort=1&priceLevel=all').then(res => {
+      console.log(res.data.result.list)
+    })
+  }
+})
